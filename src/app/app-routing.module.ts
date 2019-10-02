a46a9441a77476ca995bfd7a89f1d947223ace9b
amaury1504 <amaury.gimonnet@devinci.fr>
2019-10-02 15:40:10 +0200

    typo blanc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import { IndiceComponent } from './indice/indice.component';
 import { UrlComponent } from './url/url.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BoutiqueComponent } from './boutique/boutique.component';
 
 
 const routes: Routes = [
   { path: 'indice', component: IndiceComponent},
+  { path: 'boutique', component: BoutiqueComponent},
   { path: 'Amauryestbeau', component: UrlComponent},
   { path: 'home', component: HomeComponent},
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
